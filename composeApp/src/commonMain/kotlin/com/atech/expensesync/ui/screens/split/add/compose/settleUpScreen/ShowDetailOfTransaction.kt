8f788906a608591f2993ae122a07977285d74d7c
aiyu-ayaan <aiyuayaan22@gmail.com>
2025-02-19 23:08:15 +0530

    feat: Enhance Transaction Details with Donut Chart and Tree View
    This commit enhances the transaction details screen by adding a donut chart for visualizing split amounts and a tree view to represent the transaction structure.
    
    *   **Transaction Details UI:**
        *   Updates `ShowDetailOfTransaction` to use a `LazyColumn` for better scrolling performance.
        *   Adds a donut chart (`CreateDonutChar`) to display how the transaction is split among members.
        *   Implements a tree view (`ShowTreeStructure`) to show the flow of the transaction, indicating who paid and who owes.
        * Adds a `TreeNode` composable to display individual nodes in the transaction tree view.
        * Adds a line for connecting the nodes in the tree view.
        * Updates the transaction details screen to show a `description`, `amount`, `paid by`, date, tree view, and donut chart.
        * The tree view uses a root node for the person who paid, and then child nodes for the members who owe.
    *   **Donut Chart:**
        *   `CreateDonutChar` now visualizes split amounts with a donut chart.
        *   The chart shows each member's share with different colors.
        *   Displays a "Split" label in the center of the donut chart.
    * **Tree View**
        * The `ShowTreeStructure` now displays a tree view.
        * Displays the `paid by` first.
        * Displays the name and amount that each user owes.
    *   **General Improvements:**
        * Adds new key in the lazy column to handle the recomposition.
        * Adjusts the spacing and padding for better visual organization.
        * Enhance the style of the UI component.
        *  Update the preview for more context.
    * **Code:**
        * Updates the `ShowDetailOfTransaction` to use the new composable.
        * Updates the `DonutChart` for the new UI.
        * Adds the `ShowTreeStructure` and `TreeNode`.
        * Remove the unused code.

--- composeApp/src/commonMain/kotlin/com/atech/expensesync/ui/screens/split/add/compose/settleUpScreen/ShowDetailOfTransaction.kt
+++ composeApp/src/commonMain/kotlin/com/atech/expensesync/ui/screens/split/add/compose/settleUpScreen/ShowDetailOfTransaction.kt
@@ -1,13 +1,19 @@
 package com.atech.expensesync.ui.screens.split.add.compose.settleUpScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.twotone.AddAPhoto
 import androidx.compose.material.icons.twotone.Delete
@@ -19,10 +25,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import com.aay.compose.baseComponents.model.LegendPosition
 import com.aay.compose.donutChart.DonutChart
 import com.aay.compose.donutChart.model.PieChartData
@@ -43,81 +52,82 @@ fun ShowDetailOfTransaction(
     transaction: ExpenseTransactions,
     groupMembers: List<ExpenseGroupMembers>,
     split: List<TransactionSplit>,
+    onAddPhotoClick : () -> Unit = {},
+    onDeleteClick : () -> Unit = {},
+    onEditClick : () -> Unit = {},
     onNavigationClick: () -> Unit = {},
 ) {
     MainContainer(
-        modifier = modifier,
-        title = "Details",
-        onNavigationClick = onNavigationClick,
-        actions = {
-            IconButton(onClick = { /*TODO*/ }) {
+        modifier = modifier, title = "Details", onNavigationClick = onNavigationClick, actions = {
+            IconButton(onClick = onAddPhotoClick) {
                 Icon(imageVector = Icons.TwoTone.AddAPhoto, contentDescription = null)
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onDeleteClick) {
                 Icon(imageVector = Icons.TwoTone.Delete, contentDescription = null)
             }
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onEditClick) {
                 Icon(imageVector = Icons.TwoTone.Edit, contentDescription = null)
             }
-        }
-    ) { paddingValue ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(MaterialTheme.spacing.medium)
-                .padding(paddingValue)
-                .verticalScroll(rememberScrollState()),
+        }) { paddingValue ->
+        LazyColumn(
+            modifier = Modifier.fillMaxSize().padding(MaterialTheme.spacing.medium)
+                .padding(paddingValue)/*.verticalScroll(rememberScrollState())*/,
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
         ) {
-            val colors = OutlinedTextFieldDefaults.colors(
-                disabledTextColor = OutlinedTextFieldDefaults.colors().focusedTextColor,
-                disabledContainerColor = OutlinedTextFieldDefaults.colors().focusedContainerColor,
-                disabledBorderColor = OutlinedTextFieldDefaults.colors().focusedIndicatorColor,
-                disabledPlaceholderColor = OutlinedTextFieldDefaults.colors().focusedPlaceholderColor,
-                disabledLeadingIconColor = OutlinedTextFieldDefaults.colors().focusedLeadingIconColor,
-                disabledTrailingIconColor = OutlinedTextFieldDefaults.colors().focusedTrailingIconColor,
-                disabledLabelColor = OutlinedTextFieldDefaults.colors().focusedLabelColor,
-                disabledSupportingTextColor = OutlinedTextFieldDefaults.colors().focusedSupportingTextColor,
-            )
-            EditTextEnhance(
-                modifier = Modifier.fillMaxWidth(),
-                value = transaction.description,
-                placeholder = "Description",
-                enable = false,
-                trailingIcon = null,
-                colors = colors,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.TwoTone.Description,
-                        contentDescription = null
-                    )
-                },
-            )
+            item(key = "transaction_details") {
+                val colors = OutlinedTextFieldDefaults.colors(
+                    disabledTextColor = OutlinedTextFieldDefaults.colors().focusedTextColor,
+                    disabledContainerColor = OutlinedTextFieldDefaults.colors().focusedContainerColor,
+                    disabledBorderColor = OutlinedTextFieldDefaults.colors().focusedIndicatorColor,
+                    disabledPlaceholderColor = OutlinedTextFieldDefaults.colors().focusedPlaceholderColor,
+                    disabledLeadingIconColor = OutlinedTextFieldDefaults.colors().focusedLeadingIconColor,
+                    disabledTrailingIconColor = OutlinedTextFieldDefaults.colors().focusedTrailingIconColor,
+                    disabledLabelColor = OutlinedTextFieldDefaults.colors().focusedLabelColor,
+                    disabledSupportingTextColor = OutlinedTextFieldDefaults.colors().focusedSupportingTextColor,
+                )
+                EditTextEnhance(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = transaction.description,
+                    placeholder = "Description",
+                    enable = false,
+                    trailingIcon = null,
+                    colors = colors,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.TwoTone.Description, contentDescription = null
+                        )
+                    },
+                )
 
-            EditTextEnhance(
-                modifier = Modifier.fillMaxWidth(),
-                value = transaction.formatedAmount,
-                placeholder = "Amount",
-                enable = false,
-                trailingIcon = null,
-                colors = colors,
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.TwoTone.Money,
-                        contentDescription = null
-                    )
-                },
-                supportingText = {
-                    Text(
-                        "Paid by ${groupMembers.first { it.key.contains(transaction.paidByKey) }.name} on ${transaction.formatedDate}",
-                        style = MaterialTheme.typography.labelSmall,
-                    )
-                }
-            )
-            CreateDonutChar(
-                groupMembers = groupMembers,
-                split = split
-            )
+                EditTextEnhance(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = transaction.formatedAmount,
+                    placeholder = "Amount",
+                    enable = false,
+                    trailingIcon = null,
+                    colors = colors,
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.TwoTone.Money, contentDescription = null
+                        )
+                    },
+                    supportingText = {
+                        Text(
+                            "Paid by ${groupMembers.first { it.key.contains(transaction.paidByKey) }.name} on ${transaction.formatedDate}",
+                            style = MaterialTheme.typography.labelSmall,
+                        )
+                    })
+            }
+            item(key = "transaction_tree") {
+                ShowTreeStructure(
+                    transaction = transaction, groupMembers = groupMembers, splits = split
+                )
+            }
+            item(key = "transaction_graph") {
+                CreateDonutChar(
+                    groupMembers = groupMembers, split = split
+                )
+            }
         }
     }
 }
@@ -133,20 +143,14 @@ private fun CreateDonutChar(
         fontSize = MaterialTheme.typography.titleSmall.fontSize,
         color = MaterialTheme.colorScheme.onSurface,
     )
-    val pieChartData: List<PieChartData> =
-        split.mapIndexed { index, splitTransaction ->
-            val user = groupMembers.find { it.key == splitTransaction.memberKey }?.name
-                ?: "No Name"
-            PieChartData(
-                partName = user,
-                data = splitTransaction.amount,
-                color = colorList[index]
-            )
-        }
+    val pieChartData: List<PieChartData> = split.mapIndexed { index, splitTransaction ->
+        val user = groupMembers.find { it.key == splitTransaction.memberKey }?.name ?: "No Name"
+        PieChartData(
+            partName = user, data = splitTransaction.amount, color = colorList[index]
+        )
+    }
     DonutChart(
-        modifier = modifier
-            .fillMaxWidth()
-            .aspectRatio(1f),
+        modifier = modifier.fillMaxWidth().aspectRatio(1f),
         pieChartData = pieChartData,
         centerTitle = "Split",
         centerTitleStyle = textStyle,
@@ -159,16 +163,103 @@ private fun CreateDonutChar(
     )
 }
 
+@Composable
+private fun ShowTreeStructure(
+    modifier: Modifier = Modifier,
+    transaction: ExpenseTransactions,
+    groupMembers: List<ExpenseGroupMembers>,
+    splits: List<TransactionSplit>
+) {
+    val paidBy = groupMembers.first { it.key == transaction.paidByKey }
 
-//Column {
-//    split.forEach { split ->
-//        val user = groupMembers.first { it.key.contains(split.memberKey) }
-//        Text(
-//            text = "${user.name} owes ${split.formatedAmount}",
-//            style = MaterialTheme.typography.bodyMedium
-//        )
-//    }
-//}
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        // Root transaction node
+        Text(
+            modifier = Modifier,
+            text = "Transaction Details",
+            style = MaterialTheme.typography.titleMedium
+        )
+        // Paid By Node
+        TreeNode(
+            text = "Paid by: ${paidBy.name}",
+            amount = transaction.formatedAmount,
+            isRoot = true
+        )
+
+        // Split Members
+        splits.forEach { split ->
+            val member = groupMembers.first { it.key == split.memberKey }
+            TreeNode(
+                text = member.name,
+                amount = split.formatedAmount,
+                isRoot = false
+            )
+        }
+    }
+}
+
+@Composable
+private fun TreeNode(
+    text: String,
+    amount: String,
+    isRoot: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(start = if (isRoot) 0.dp else 16.dp)
+            .padding(vertical = 2.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (!isRoot) {
+            // Vertical line for child nodes
+            Box(
+                modifier = Modifier
+                    .width(16.dp)
+                    .height(24.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(2.dp)
+                        .fillMaxHeight()
+                        .background(MaterialTheme.colorScheme.outline.copy(alpha = 0.5f))
+                        .align(Alignment.CenterStart)
+                )
+            }
+
+            // Horizontal dash for connection
+            Box(
+                modifier = Modifier
+                    .width(16.dp)
+                    .height(24.dp)
+            ) {
+                Box(
+                    modifier = Modifier
+                        .width(16.dp)
+                        .height(2.dp)
+                        .background(MaterialTheme.colorScheme.outline.copy(alpha = 0.5f))
+                        .align(Alignment.CenterStart)
+                )
+            }
+        }
+
+        // Node content
+        Surface(
+            modifier = Modifier.padding(end = 8.dp),
+        ) {
+            Text(
+                text = " $text owes $amount",
+                style = if (isRoot) MaterialTheme.typography.bodyLarge else MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(start = 4.dp, bottom = 4.dp)
+                    .width(IntrinsicSize.Max)
+            )
+        }
+    }
+}
+
 
 @Preview
 @Composable
@@ -181,8 +272,7 @@ private fun SettleUpScreenPreview() {
                 amount = 300.0,
                 description = "Test",
                 paidByKey = "xvz$23",
-            ),
-            groupMembers = listOf(
+            ), groupMembers = listOf(
                 ExpenseGroupMembers(
                     uid = "xyz",
                     name = "Ayaan",
@@ -190,16 +280,14 @@ private fun SettleUpScreenPreview() {
                     email = "ayaan@expanseSyncTest",
                     pic = "https://www.google.com",
                     groupId = "sdf"
-                ),
-                ExpenseGroupMembers(
+                ), ExpenseGroupMembers(
                     uid = "abc",
                     name = "Anshu",
                     key = "abc$56",
                     email = "anshu@expanseSyncTest",
                     pic = "https://www.google.com",
                     groupId = "sdf"
-                ),
-                ExpenseGroupMembers(
+                ), ExpenseGroupMembers(
                     uid = "abx",
                     name = "Jai",
                     key = "xvz$60",
@@ -207,21 +295,18 @@ private fun SettleUpScreenPreview() {
                     pic = "https://www.google.com",
                     groupId = "sdf"
                 )
-            ),
-            split = listOf(
+            ), split = listOf(
                 TransactionSplit(
                     transactionId = "transactionID123",
                     memberKey = "xvz$23",
                     amount = 100.0,
                     paidBy = "xvz$23"
-                ),
-                TransactionSplit(
+                ), TransactionSplit(
                     transactionId = "transactionID123",
                     memberKey = "abc$56",
                     amount = 100.0,
                     paidBy = "abc$56"
-                ),
-                TransactionSplit(
+                ), TransactionSplit(
                     transactionId = "transactionID123",
                     memberKey = "xvz$60",
                     amount = 100.0,
